55bb31d3aa5cdd20976b6e3514c6df1e5a6cd736
Nick Burt <nbur4556@gmail.com>
2024-05-21 18:41:53 -0500

    create initial task table component

---
+++ src/components/TaskTable.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Task } from '../types/Task.ts'
+
+const props = defineProps<{ tasks: Array<Task> }>()
+
+console.log(props.tasks)
+</script>
+
+<template>
+  <h1>Task Table</h1>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-console.log('Hello world')
+import TaskTable from '../components/TaskTable.vue'
+import type { Task } from '../types/Task.ts'
+
+const tasks: Array<Task> = []
 </script>
 
 <template>
   <main>
     <h1>Hello World</h1>
+
+    <TaskTable v-bind:tasks="tasks" />
   </main>
 </template>
