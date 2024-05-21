0e5ea24395858677dab4b808e62d3cbc13937955
Nick Burt <nbur4556@gmail.com>
2024-05-21 18:03:47 -0500

    remove all unnecessary assets and src for a hello world

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+console.log('Hello world')
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello World</h1>
   </main>
 </template>
