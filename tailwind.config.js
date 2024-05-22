a8e425e496bae3d240f3f94448cf678a6abbaace
Nick Burt <nbur4556@gmail.com>
2024-05-21 21:01:39 -0500

    add additional themes for future theme switching availability

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,11 @@
 export default {
   content: ["./src/**/*.{js,ts,vue}"],
   theme: {
-    extend: {
-      colors: {
-        // TODO: match colors to daisyui themes
-        statustodo: "#5a5a5a5a",
-        statuscurrent: "#28456c",
-        statusbin: "#6e3630",
-        statusdone: "#28593f",
-        priorityhigh: "#6e3630",
-        prioritymid: "#854c1d",
-        prioritylow: "#28456c",
-        typebug: "#6e3630",
-        typeimprovement: "#854c1d",
-        typefeature: "#28456c",
-        appassigned: "#28593f",
-        appsuggestion: "#854c1d",
-        appunassigned: "#28456c",
-        appdeclined: "#6e3630",
-      },
-    },
+    extend: {},
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ["business"]
+    themes: ["dark", "light", "business"]
   }
 }
 
