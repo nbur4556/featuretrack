f2b1ca0b233b0d351352396fbdb406142d75c1a1
Nick Burt <nbur4556@gmail.com>
2024-05-21 19:44:25 -0500

    create table headers

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -8,6 +8,14 @@ const { tasks } = defineProps<{ tasks: Array<Task> }>()
 
 <template>
   <table class="table table-sm">
+    <thead>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Status</th>
+      <th>Priority</th>
+      <th>Approval</th>
+      <th>Type</th>
+    </thead>
     <tbody>
       <TaskTableRow v-for="task in tasks" v-bind:task="task" />
     </tbody>
