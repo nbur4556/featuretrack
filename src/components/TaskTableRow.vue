c28353a56249c86463b46e679a3a324637dfc0f4
Nick Burt <nbur4556@gmail.com>
2024-05-21 19:52:15 -0500

    style page and heading

--- src/components/TaskTableRow.vue
+++ src/components/TaskTableRow.vue
@@ -7,11 +7,23 @@ const { task } = defineProps<{ task: Task }>()
 
 <template>
   <tr>
-    <td><div class="font-bold">{{ task.name }}</div></td>
-    <td><div>{{ task.description }}</div></td>
-    <td><div class='badge badge-info'>{{ task.status }}</div></td>
-    <td><div class='badge badge-info badge-outline'>{{ task.priority }}</div></td>
-    <td><div class='badge badge-info badge-outline'>{{ task.approval }}</div></td>
-    <td><div class='badge badge-info badge-outline'>{{ task.type }}</div></td>
+    <td>
+      <div class="font-bold">{{ task.name }}</div>
+    </td>
+    <td>
+      <div>{{ task.description }}</div>
+    </td>
+    <td>
+      <div class="badge badge-info">{{ task.status }}</div>
+    </td>
+    <td>
+      <div class="badge badge-info badge-outline">{{ task.priority }}</div>
+    </td>
+    <td>
+      <div class="badge badge-info badge-outline">{{ task.approval }}</div>
+    </td>
+    <td>
+      <div class="badge badge-info badge-outline">{{ task.type }}</div>
+    </td>
   </tr>
 </template>
